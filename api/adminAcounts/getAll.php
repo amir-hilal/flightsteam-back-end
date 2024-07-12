0d171122b77c2bab2274614c42ddacc42c664751
Ali Aljawad <alialjawad2k1@gmail.com>
2024-07-13 01:59:14 +0300

    created crud operations for table adminAcounts

---
+++ api/adminAcounts/getAll.php
@@ -0,0 +1,17 @@
+<?php
+require "../../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $stmt = $conn->prepare('SELECT * FROM AdminAccounts;');
+    try {
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $admins = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode(["admins" => $admins, "status" => "success"]);
+    } catch (Exception $e) {
+        echo json_encode(["error" => $stmt->error]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
+?>
---
+++ api/adminAcounts/update.php
@@ -0,0 +1,24 @@
+<?php
+require "../../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $admin_id = $_POST["admin_id"];
+    $first_name = $_POST["first_name"];
+    $middle_name = $_POST["middle_name"];
+    $last_name = $_POST["last_name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $role = $_POST["role"];
+
+    $stmt = $conn->prepare('UPDATE AdminAccounts SET first_name = ?, middle_name = ?, last_name = ?, email = ?, password = ?, role = ? WHERE admin_id = ?;');
+    $stmt->bind_param('ssssssi', $first_name, $middle_name, $last_name, $email, $password, $role, $admin_id);
+    try {
+        $stmt->execute();
+        echo json_encode(["message" => "admin account updated", "status" => "success"]);
+    } catch (Exception $e) {
+        echo json_encode(["error" => $stmt->error]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
+?>
