11ec45eb5e49827ec799e2f617dcef2225a51556
Ali Aljawad <alialjawad2k1@gmail.com>
2024-07-13 13:49:21 +0300

    added CRUD operations for table taxi

---
+++ api/taxis/delete.php
@@ -0,0 +1,18 @@
+<?php
+require "../../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $taxi_id = $_POST["taxi_id"];
+
+    $stmt = $conn->prepare('DELETE FROM Taxis WHERE taxi_id = ?;');
+    $stmt->bind_param('i', $taxi_id);
+    try {
+        $stmt->execute();
+        echo json_encode(["message" => "taxi deleted", "status" => "success"]);
+    } catch (Exception $e) {
+        echo json_encode(["error" => $stmt->error]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
+?>
---
+++ api/taxis/update.php
@@ -0,0 +1,22 @@
+<?php
+require "../../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $taxi_id = $_POST["taxi_id"];
+    $company_name = $_POST["company_name"];
+    $car_type = $_POST["car_type"];
+    $price_per_km = $_POST["price_per_km"];
+    $available = $_POST["available"];
+
+    $stmt = $conn->prepare('UPDATE Taxis SET company_name = ?, car_type = ?, price_per_km = ?, available = ? WHERE taxi_id = ?;');
+    $stmt->bind_param('ssdii', $company_name, $car_type, $price_per_km, $available, $taxi_id);
+    try {
+        $stmt->execute();
+        echo json_encode(["message" => "taxi updated", "status" => "success"]);
+    } catch (Exception $e) {
+        echo json_encode(["error" => $stmt->error]);
+    }
+} else {
+    echo json_encode(["error" => "Wrong request method"]);
+}
+?>
